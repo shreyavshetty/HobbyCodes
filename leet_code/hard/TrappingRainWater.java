75fce9160bec719e85851c47cecf54384d7382ea
shreyavshetty@gmail.com <shreyav4@gmail.com>
2020-09-19 19:42:02 +0530

    Added Trapping Rain Water space cmpxty O(1)

--- leet_code/hard/TrappingRainWater.java
+++ leet_code/hard/TrappingRainWater.java
@@ -1,4 +1,40 @@
 public class TrappingRainWater {
+
+    // Time complexity : O(n)
+    // Space Complexity : O(1)
+    public int trap1(int[] height) {
+        
+        if(height==null || height.length==0)
+            return 0;
+        
+        int n = height.length;
+        int left = 0;
+        int right = n-1;
+        int left_max = 0;
+        int right_max = 0;
+        int result = 0;
+        while(left<right)
+        {
+            if(height[right]>height[left])
+            {
+                if(height[left]>=left_max)
+                    left_max = height[left];
+                else
+                    result += left_max - height[left];
+                left++;
+            }
+            else
+            {
+                if(height[right]>=right_max)
+                    right_max = height[right];
+                    else
+                        result += right_max -  height[right];
+                    right--;
+                }
+            }
+            return result;
+        }
+    
     // Time complexity : O(n)
     // Space Complexity : O(n)
     public int trap(int[] height) {
